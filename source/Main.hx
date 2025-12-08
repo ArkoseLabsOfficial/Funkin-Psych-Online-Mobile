b3cda235a64c7215e931b00adfc2230e07f55583
Prohack101010 <28umutmehmet28@gmail.com>
2025-12-08 17:10:17 +0000

    uhh

--- source/Main.hx
+++ source/Main.hx
@@ -16,7 +16,6 @@ import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
 import states.TitleState;
-import mobile.backend.MobileScaleMode;
 
 #if linux
 import lime.graphics.Image;
