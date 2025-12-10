8bb4a39894736c4608588661b68b07b3981e2a87
Prohack101010 <28umutmehmet28@gmail.com>
2025-12-10 03:24:42 +0000

    fuck

--- source/Main.hx
+++ source/Main.hx
@@ -112,13 +112,6 @@ class Main extends Sprite
 		#end
 		backend.CrashHandler.init();
 
-		// Assets folder
-		#if mobile
-		//Library folder needs to be copy with specificef folder idk why
-		if (!StorageUtil.areAssetsCopied("assets/"))
-			StorageUtil.copyAssetsFromAPK("assets/");
-		#end
-
 		#if (cpp && windows)
 		backend.Native.fixScaling();
 		#end
@@ -198,7 +191,7 @@ class Main extends Sprite
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(game.width, game.height, game.initialState, #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
